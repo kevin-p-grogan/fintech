c11990321ac6ac835a968c352576e42bf1d4d130
Kevin Grogan <kevin.grogan@omni.sc>
2021-07-10 11:09:45 -0700

    Added capability to import raw portfolio data.

--- src/data_munger.py
+++ src/data_munger.py
@@ -1,11 +1,18 @@
 from datetime import datetime, timedelta
+from typing import Any
+import locale
 
+import numpy as np
 import pandas as pd
+
+locale.setlocale(locale.LC_ALL, 'en_us')
 
 
 class DataMunger:
     _num_days_time_horizon: float
     _analysis_column: str
+
+    NUM_PORTFOLIO_DATA_COLS = 7
 
     def __init__(self, num_days_time_horizon: float = 365., anaysis_column: str = "Adj Close"):
         self._num_days_time_horizon = num_days_time_horizon
@@ -45,4 +52,36 @@ class DataMunger:
         days_in_year = 365.0
         times = pd.Series([delta.days / days_in_year for delta in deltas], index=dates)
         return times
+
+    @staticmethod
+    def load_portfolio_data(file_path: str, index_col: str = "Symbol") -> pd.DataFrame:
+        raw_sequence = pd.read_csv(file_path, header=None, comment="#")
+        raw_sequence = np.squeeze(raw_sequence.to_numpy())
+        header = raw_sequence[:DataMunger.NUM_PORTFOLIO_DATA_COLS]
+        data = raw_sequence[DataMunger.NUM_PORTFOLIO_DATA_COLS:]
+        DataMunger._check_data_format(data, header)
+        data = data.reshape((-1, DataMunger.NUM_PORTFOLIO_DATA_COLS))
+        df = pd.DataFrame(data, columns=header)
+        df = df.set_index(index_col)
+        df = df.applymap(DataMunger._convert_numerics)
+        return df
+
+    @staticmethod
+    def _check_data_format(data: np.ndarray, header: np.ndarray):
+        data_doesnt_match_header = len(data) % len(header) != 0
+        if data_doesnt_match_header:
+            raise ValueError(f"Found {len(header)} header rows and {len(data)} data rows. "
+                             f"Ensure that the data has been properly copied or that formatting hasn't changed.")
+
+    @staticmethod
+    def _convert_numerics(datum: Any) -> Any:
+        """Converts numeric and currency strings to floats."""
+        if isinstance(datum, str):
+            datum = datum.strip("$")
+            try:
+                datum = locale.atof(datum)
+            except ValueError:
+                pass
+
+        return datum
 
