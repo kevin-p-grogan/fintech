6750bc780a14d0a55743992b555d00a66648dca2
Kevin Grogan <kevin.grogan@omni.sc>
2021-12-19 12:06:05 -0800

    Added capability to comment out securities.

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -4,7 +4,7 @@ from src.config import Config
 
 
 def main(cfg: Config):
-    metadata = pd.read_csv(cfg.METADATA_FILEPATH)
+    metadata = pd.read_csv(cfg.METADATA_FILEPATH, comment="#")
     ticker_symbols = " ".join(metadata[cfg.TICKER_SYMBOL_COLUMN])
     data = yf.download(tickers=ticker_symbols, period=cfg.PERIOD, interval=cfg.INTERVAL, group_by="ticker")
     data.to_pickle(cfg.OUTPUT_FILEPATH)
